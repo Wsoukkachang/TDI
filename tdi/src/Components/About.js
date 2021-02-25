a3d64421f4d2aa55444f38ff3eb1c0b63a5ae94a
William Soukkachang <Wsoukkachang@yahoo.com>
2021-02-25 11:11:22 -0600

    Changed About info and animation.

--- tdi/src/Components/About.js
+++ tdi/src/Components/About.js
@@ -9,18 +9,28 @@ export default class About extends Component {
       <div className="aboutContainer">
         <h1 className="aboutDiv">About Us</h1>
         <Divider variant="fullWidth" />
+        <span className="aboutParagraph">
+          Texas Drone Imaging is a family owned aerial services company that
+          serves the Dallas-Fort Worth Metropolitan Area.
+          <br />
+          <br />
+          Our mission is to provide our clients with a new innovative
+          perspective to engage their audience, through the use of the latest
+          drone technology.
+          <br />
+          <br />
+          Client satisfaction is our number one goal. Where we look to go above
+          and beyond by specifically tailoring our services to meet individual
+          client needs, while maintaining exceptional quality and service
+          standards.
+          <br />
+          <br />
+        </span>
         <Typist>
-          <span className="aboutParagraph">
-            SOLUTIONS WITH NEW PERSPECTIVES. OUR COMPANY IS CONSTANTLY EVOLVING
-            AND GROWING.
-            <br />
-            WE PROVIDE A WIDE RANGE OF AERIAL SERVICES.
-            <br />
-            OUR MISSION IS TO PROVIDE THE BEST SOLUTION THAT HELPS EVERYONE.
-            <br />
-            IF YOU WOULD LIKE TO CONTACT US,
-            <br />
-            PLEASE CLICK ON THE BUTTON ABOVE, OR BELOW.
+          <Typist.Delay ms={5500} />
+          <span className="aboutParagraphType">
+            Here at Texas Drone Imaging, we take pride in our work and aim to
+            show this through the services we provide.
           </span>
         </Typist>
       </div>
